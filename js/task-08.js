11e6bce0730abcee2e64bb031219521629144de5
MarynaChicherina <marinachicherina.ua@gmail.com>
2023-02-23 14:17:50 +0200

    Add task_8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('form.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit (event){
+    event.preventDefault();
+
+    const {
+        elements: { email, password }
+      } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        window.alert("Будь ласка заповніть поля форми!");
+      } else
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+    
+    event.currentTarget.reset();  
+};
