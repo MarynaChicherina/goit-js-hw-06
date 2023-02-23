348cb808167b78e1762254aedb5be49f4271c378
MarynaChicherina <marinachicherina.ua@gmail.com>
2023-02-23 16:21:40 +0200

    Add task 9

--- js/task-08.js
+++ js/task-08.js
@@ -4,15 +4,19 @@ form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit (event){
     event.preventDefault();
+    const formElems = event.currentTarget.elements;
+    const email = formElems.email.value;
+    const password = formElems.password.value;
 
-    const {
-        elements: { email, password }
-      } = event.currentTarget;
-
-    if (email.value === "" || password.value === "") {
-        window.alert("Будь ласка заповніть поля форми!");
-      } else
-    console.log(`Email: ${email.value}, Password: ${password.value}`);
-    
+    const formData = {
+        email,
+        password,
+    }
+    if (email === "" || password === "") {
+        alert("Будь ласка заповніть поля форми!");
+      } else if(email.value !== "" && password.value !== ""){
+        console.log(formData);
+    };
+  
     event.currentTarget.reset();  
 };
--- js/task-09.js
+++ js/task-09.js
@@ -2,4 +2,18 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+};
+
+
+const btn = document.querySelector('button.change-color');
+const span = document.querySelector('span.color');
+
+btn.addEventListener('click', onButtonChangeColor);
+
+function onButtonChangeColor (){
+  const color = getRandomHexColor();
+  
+  span.textContent = color;
+  document.body.style.background =  span.textContent;
 }
+
